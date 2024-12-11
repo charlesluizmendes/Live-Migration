400a6d2532a340dc22d8e5483f2f7b39d481c972
Charles Mendes <charlesluizmendes@gmail.com>
2024-12-11 13:36:43 -0300

    Update migration.sh

--- migration.sh
+++ migration.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
  
-#Apaga conteúdo do diretório checkpoint se ele existir, para evitar erros
+# Apaga conteúdo do diretório checkpoint se ele existir, para evitar erros
 if [ -e /tmp/checkpoint/ ]; then
   rm -rf /tmp/checkpoint/*
 fi
@@ -9,7 +9,7 @@ usage() {
   exit 1
 }
  
-#Verificação de erros e argumentos passados na hora de chamar a execução do script
+# Verificação de erros e argumentos passados na hora de chamar a execução do script
 if [ "$(id -u)" != "0" ]; then
   echo "ERROR: Must run as root."
   usage
@@ -18,25 +18,30 @@ if [ "$#" != "2" ]; then
   echo "Bad number of args."
   usage
 fi
+
+# Variáveis de execução
 name=$1
 host=$2
 checkpoint_dir=/tmp/checkpoint
  
-#Função responsável por sincronizar as pastas onde estão os arquivos dos containers entre os servidores
+# Função responsável por sincronizar as pastas onde estão os arquivos dos containers entre os servidores
 do_rsync() {
   rsync -aAXHltzh --progress --numeric-ids --devices --rsync-path="sudo rsync" $1 $host:$1
 }
  
-# we assume the same lxcpath on both hosts, that is bad.
+# Cria um path para ser utilizado no container de destino
 LXCPATH=$(lxc-config lxc.lxcpath)
  
-#Ação responsável por criar o checkpoint
+# Ação responsável por criar o checkpoint
 lxc-checkpoint -n $name -D $checkpoint_dir
  
-#Ação responsável por fazer a sincronização entre os servidores
+# Ação responsável por fazer a sincronização entre os servidores
 do_rsync $LXCPATH/$name/
 do_rsync $checkpoint_dir/
  
-#Executa um comando via ssh para “levantar” o container no servidor para o qual ele foi migrado
+# Executa um comando via ssh para “levantar” o container no servidor para o qual ele foi migrado
 ssh $host "sudo lxc-checkpoint -r -n $name -D $checkpoint_dir"
 ssh $host "sudo lxc-start -n $name"
+
+# Printo de resultados
+echo "Migração do container $name para $host concluída com sucesso!"
